0c2c3cd9e8d22c5a5d244d7d75601ae3be289fc6
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-25 13:52:55 -0300

    Refactoring: Implementing Clean Architecture: Interface Adapters

---
+++ backend/checkout/src/infra/gateway/currency_gateway_http.py
@@ -0,0 +1,12 @@
+from checkout.src.application.gateway.currency_gateway import CurrencyGateway
+from checkout.src.infra.http.http_client import HttpClient
+
+
+class CurrencyGatewayHttp(CurrencyGateway):
+    def __init__(self, http_client: HttpClient) -> None:
+        self.http_client = http_client
+
+    async def get_currencies(self) -> dict:
+        response = await self.http_client.get('http://localhost:3003/currencies')
+        response.raise_for_status()
+        return response.json()
